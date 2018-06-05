5bf9db92c9ff18bd209636e77db6c890e001fb4d
Dragan Stankovi? <E381-2014@ftn.ns>
2018-06-05 09:18:58 +0200

    Entities added

--- RentApp/Migrations/Configuration.cs
+++ RentApp/Migrations/Configuration.cs
@@ -6,7 +6,9 @@ namespace RentApp.Migrations
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Data.Entity.Validation;
     using System.Linq;
+    using System.Text;
 
     internal sealed class Configuration : DbMigrationsConfiguration<RentApp.Persistance.RADBContext>
     {
@@ -72,7 +74,19 @@ namespace RentApp.Migrations
 
             );
 
-            context.SaveChanges();
+            var tov1 = new TypeOfVehicle() { Name = "Karavan" };
+            var tov2 = new TypeOfVehicle() { Name = "Limuzina" };
+
+            var v1 = new Vehicle() { Manufactor = "Zastava", Model = "101", PricePerHour = 10.4m, Type = tov1, Year = 1969 };
+            var v2 = new Vehicle() { Manufactor = "Zastava", Model = "Yugo Koral", PricePerHour = 10.4m, Type = tov2, Year = 1988 };
+
+            var b1 = new Branch() { Address = "Sime Solaje 24", Latitude = 12321312323.2, Longitude = 31212323123.2 };
+
+            var s1 = new Service() { Name = "Zastava rent", Email = "kontakt@zastava.rs", Description = "Nepobedivi", Branches = new System.Collections.Generic.List<Branch>() { b1 }, Vehicles = new System.Collections.Generic.List<Vehicle>() { v1, v2 } };
+
+            context.Services.Add(s1);
+
+            SaveChanges(context);
 
             var userStore = new UserStore<RAIdentityUser>(context);
             var userManager = new UserManager<RAIdentityUser>(userStore);
@@ -94,6 +108,30 @@ namespace RentApp.Migrations
                 userManager.Create(user);
                 userManager.AddToRole(user.Id, "AppUser");
 
+            }
+        }
+        private static void SaveChanges(DbContext context)
+        {
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                var sb = new StringBuilder();
+                foreach (var failure in ex.EntityValidationErrors)
+                {
+                    sb.AppendFormat("{0} failed validation\n", failure.Entry.Entity.GetType());
+                    foreach (var error in failure.ValidationErrors)
+                    {
+                        sb.AppendFormat("- {0} : {1}", error.PropertyName, error.ErrorMessage);
+                        sb.AppendLine();
+                    }
+                }
+                throw new DbEntityValidationException(
+                    "Entity Validation Failed - errors follow:\n" +
+                    sb.ToString(), ex
+                );
             }
         }
     }
