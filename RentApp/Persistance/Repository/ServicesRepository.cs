ee84e60c66240e74e62dbc866579ce90b72965c7
Dragan Stankovi? <E381-2014@ftn.ns>
2018-06-05 12:44:33 +0200

    Added some repositories

---
+++ RentApp/Persistance/Repository/ServicesRepository.cs
@@ -0,0 +1,23 @@
+﻿using RentApp.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RentApp.Persistance.Repository
+{
+    public class ServicesRepository : Repository<Service, int>, IServicesRepository
+    {
+        public ServicesRepository(DbContext context) : base(context)
+        {
+        }
+
+        public IEnumerable<Service> GetAll(int pageIndex, int pageSize)
+        {
+            return RadbContext.Services.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+        }
+
+        protected RADBContext RadbContext { get { return context as RADBContext; } }
+    }
+}
