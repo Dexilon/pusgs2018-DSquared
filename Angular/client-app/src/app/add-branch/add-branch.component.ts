1b3c7bcd3b56984ba0827df899cb476379ce2afd
Dejan Pavlovi? <E3110-2014@ftn.ns>
2018-06-14 08:54:24 +0200

    Added branch logo upload and download

--- Angular/client-app/src/app/add-branch/add-branch.component.ts
+++ Angular/client-app/src/app/add-branch/add-branch.component.ts
@@ -5,6 +5,10 @@ import {BranchServiceService} from '../branchService/branch-service.service';
 import { Observable } from 'rxjs/internal/Observable';
 import {Service} from '../models/service'
 import {ServiceServiceService} from '../serviceService/service-service.service';
+import {FileUploader,FileSelectDirective } from 'ng2-file-upload/ng2-file-upload';
+
+
+const URL = 'http://localhost:51680/api/Upload/user/PostBranchImage';
 
 @Component({
   selector: 'app-add-branch',
@@ -16,7 +20,17 @@ export class AddBranchComponent implements OnInit {
 
   branches: Branch[];
   services: Service[];
-    constructor(private branchServiceService: BranchServiceService, private serviceServiceService: ServiceServiceService) { }
+  public uploader: FileUploader = new FileUploader({url: URL, itemAlias: 'photo'});
+  url: string;
+
+    constructor(private branchServiceService: BranchServiceService, private serviceServiceService: ServiceServiceService) { 
+    this.uploader.onAfterAddingFile = (file) => {file.withCredentials = false;};
+    this.uploader.onCompleteItem = (item: any, response: any,status: any, headers: any) => {
+        this.url=JSON.parse(response);        
+    };
+  }
+
+  uploadFile: any;
   
     ngOnInit() {
       this.serviceServiceService.getMethodService()
@@ -39,6 +53,7 @@ export class AddBranchComponent implements OnInit {
   
     onSubmit(branch: Branch, form: NgForm) {
         console.log(branch);
+        branch.Logo = this.url;
         this.branchServiceService.postMethodBranch(branch)
         .subscribe(
           data => {
@@ -52,6 +67,13 @@ export class AddBranchComponent implements OnInit {
   
       checkIfLogged() {
         return localStorage.jwt;
+      }
+
+      handleUpload(data): void{
+        if(data && data.response){
+          data = JSON.parse(data.response);
+          this.uploadFile = data;
+        }
       }
 
 }
